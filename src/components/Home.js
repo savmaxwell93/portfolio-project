245ab952c849cca12ce212698a18a0329abeaed2
Savannah <savmaxwell@gmail.com>
2022-05-19 13:20:38 -0600

    homepage WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Home = (props) => {
     return (
-        <div className='App-header'>
+        <div className='home-container'>
             <h1>Hi there! I'm Savannah Maxwell.</h1>
             <p>Full stack web development student. Rock climber. Backpacker. Gamer. Certified Veterinary Technician</p>
         </div>

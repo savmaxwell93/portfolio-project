d974e4b37611deb0c3b824f08fbaf0482c89580a
Savannah <savmaxwell@gmail.com>
2022-05-19 13:16:39 -0600

    changed styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Home = (props) => {
     return (
-        <div>
+        <div className='App-header'>
             <h1>Hi there! I'm Savannah Maxwell.</h1>
             <p>Full stack web development student. Rock climber. Backpacker. Gamer. Certified Veterinary Technician</p>
         </div>

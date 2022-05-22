df8b16c917cc1e765bcc69e1115e190b23f8f0c8
Savannah <savmaxwell@gmail.com>
2022-05-22 11:40:19 -0600

    added information to contact and projects pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Contact = (props) => {
     return (
         <div>
-            <h1>Obligatory Title</h1>
+            <h1>Contact Me!</h1>
+            <address>
+                <a href="mailto:savmaxwell93@gmail.com">Email</a>
+                <a href="https://www.linkedin.com/in/savannahmaxwell93/" rel="noopener noreferrer" target="_blank">LinkedIn</a>
+            </address>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Projects = (props) => {
     return (
         <div>
-            <h1>Obligatory Title</h1>
+            <h1>Projects I'm Working On</h1>
+            <a href="https://github.com/savmaxwell93" target="_blank" rel="noopener noreferrer">Check out my github!</a>
+            <div>
+                <a href="https://dnd-spell-grabber.vercel.app/" target="_blank" rel="noopener noreferrer">DnD Spell Grabber</a>
+                <p>A small search engine to display spells from Dungeons and Dragons!</p>
+            </div>
         </div>
     )
 }

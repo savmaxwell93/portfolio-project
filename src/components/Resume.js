1d6039018b0693d4c4faaa954a6a8b20d6bcac7c
Savannah <savmaxwell@gmail.com>
2022-06-02 10:50:27 -0600

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ const Resume = (props) => {
     return (
         <div>
             <h1>View My Resume Here!</h1>
-            <iframe src="https://docs.google.com/document/d/e/2PACX-1vQWqR91s3HUpsmwdrPuQIOnADOO-MTRitM0wPrwX9dFIyV15R0ooFrWm-wlhjuytR6wDkNDDqNN18wM/pub?embedded=true"></iframe>
+            <iframe src="https://docs.google.com/document/d/e/2PACX-1vTY6lsw7MW5hkOd_lszNt6y__2QXTsPPvYubAeGmO5Qb8T3eJDCM83Xf7a-Pmy14Iw5Od6N-qnz-XWs/pub?embedded=true"></iframe>
         </div>
     )
 }

ef5d4f094a62b2a7b0486237283ef26e931eef90
Savannah <savmaxwell@gmail.com>
2022-06-02 10:33:16 -0600

    added activities

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -3,7 +3,81 @@ import React from 'react';
 const Activities = (props) => {
     return (
         <div>
-            <h1>Obligatory Title</h1>
+            <h1>Things I've Been Up To!</h1>
+            <div>
+                <h2>2019</h2>
+                <div>
+                    <h3>St. Augustine, Fl - Feburary 2019</h3>
+                </div>
+                <div>
+                    <h3>Table Rock - April 2019</h3>
+                </div>
+                <div>
+                    <h3>Bruneau Dunes - May 2019</h3>
+                </div>
+                <div>
+                    <h3>Tampa, Fl - May 2019</h3>
+                </div>
+                <div>
+                    <h3>Hell Roaring Lake/Lake Imogene - June 2019</h3>
+                </div>
+                <div>
+                    <h3>Stack Rock - July 2019</h3>
+                </div>
+                <div>
+                    <h3>Alpine Lake/Sawtooth Lake - July 2019</h3>
+                </div>
+                <div>
+                    <h3>Disrupt Festival - July 2019</h3>
+                </div>
+                <div>
+                    <h3>In This Moment - August 2019</h3>
+                </div>
+                <div>
+                    <h3>White Clouds Loop - August 2019</h3>
+                </div>
+                <h2>2020</h2>
+                <div>
+                    <h3>Stack Rock - March 2020</h3>
+                    <p>Not many pictures from this one, turns out mountains in early March is a mistake.. oops!</p>
+                </div>
+                <div>
+                    <h3>Black Cliffs - May 2020</h3>
+                </div>
+                <div>
+                    <h3>Alpine Lake - June 2020</h3>
+                </div>
+                <div>
+                    <h3>Stack Rock - August 2020</h3>
+                </div>
+                <h2>2021</h2>
+                <div>
+                    <h3>Somewhere Up Bogus Basin Rd</h3>
+                    <p>This was a failed cross country skiing attempt. My ski broke about 5 minutes in and we had to abandon ship for the day!</p>
+                </div>
+                <div>
+                    <h3>Shane's Trail - April 2021</h3>
+                </div>
+                <div>
+                    <h3>St. Augustine to Idaho Cross Country Trip - June 2021</h3>
+                </div>
+                <div>
+                    <h3>Hiawatha Bike Trail - July 2021</h3>
+                </div>
+                <div>
+                    <h3>Green Belt - December 2021</h3>
+                </div>
+                <h2>2022</h2>
+                <div>
+                    <h3>Adventure Challenge - January 2022</h3>
+                </div>
+                <div>
+                    <h3>2 Year Anniversary - February 2022</h3>
+                </div>
+                <div>
+                    <h3>Adventure Challenge - March 2022</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = (props) => {
     return (
         <div>
-            <h1>Obligatory Title</h1>
+            <h1>View My Resume Here!</h1>
             <iframe src="https://docs.google.com/document/d/e/2PACX-1vQWqR91s3HUpsmwdrPuQIOnADOO-MTRitM0wPrwX9dFIyV15R0ooFrWm-wlhjuytR6wDkNDDqNN18wM/pub?embedded=true"></iframe>
         </div>
     )
